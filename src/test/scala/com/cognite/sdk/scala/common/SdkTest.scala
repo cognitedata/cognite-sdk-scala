104514f650f3ed05939bb437bcd1628bdd970cda
Joel Wilsson <joel.wilsson@cognite.com>
2019-05-02 17:41:52 +0200

    Use TEST_API_KEY_READ for tests

--- src/test/scala/com/cognite/sdk/scala/common/SdkTest.scala
+++ src/test/scala/com/cognite/sdk/scala/common/SdkTest.scala
@@ -4,7 +4,7 @@ import com.softwaremill.sttp.{HttpURLConnectionBackend, Id, SttpBackend}
 import org.scalatest.{FlatSpec, Matchers}
 
 abstract class SdkTest extends FlatSpec with Matchers {
-  private val apiKey = System.getenv("COGNITE_API_KEY")
+  private val apiKey = System.getenv("TEST_API_KEY_READ")
   implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
   implicit val auth: Auth = ApiKeyAuth(apiKey)
 }
