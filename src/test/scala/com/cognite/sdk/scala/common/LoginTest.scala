0dd6df79e1c444a58ea9af4bb3754b1735b00a22
Joel Wilsson <joel.wilsson@gmail.com>
2019-06-03 22:40:12 +0200

    Make LoginTest not depend on any specific client version

--- src/test/scala/com/cognite/sdk/scala/common/LoginTest.scala
+++ src/test/scala/com/cognite/sdk/scala/common/LoginTest.scala
@@ -1,11 +1,10 @@
 package com.cognite.sdk.scala.common
 
-import com.cognite.sdk.scala.v0_6.Client
-
 class LoginTest extends SdkTest {
   it should "read login status" in {
-    val client = new Client()
-    val status = client.login.status()
-    println(status.unsafeBody) // scalastyle:ignore
+    val login = new Login()
+    val status = login.status().unsafeBody
+    status.loggedIn should be (true)
+    status.project should not be empty
   }
 }
