08c9a65a13e772883b298ba6b07bd9646159e75d
Joel Wilsson <joel.wilsson@gmail.com>
2019-06-09 14:32:45 +0200

    Parse error replies when creating files in v1

--- src/main/scala/com/cognite/sdk/scala/v1_0/files.scala
+++ src/main/scala/com/cognite/sdk/scala/v1_0/files.scala
@@ -1,8 +1,9 @@
 package com.cognite.sdk.scala.v1_0
 
-import com.cognite.sdk.scala.common.{Auth, Items, WithId}
+import com.cognite.sdk.scala.common.{Auth, CdpApiError, CogniteId, EitherDecoder, Items, WithId}
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.circe._
+import io.circe.Decoder
 import io.circe.generic.auto._
 
 final case class File(
@@ -34,16 +35,19 @@ class Files[F[_]](implicit auth: Auth, sttpBackend: SttpBackend[F, _])
     with ResourceV1[F] {
   override val baseUri = uri"https://api.cognitedata.com/api/v1/projects/playground/files"
 
+  implicit val errorOrFileDecoder: Decoder[Either[CdpApiError[CogniteId], File]] =
+    EitherDecoder.eitherDecoder[CdpApiError[CogniteId], File]
   override def createItems(items: Items[CreateFile]): F[Response[Seq[File]]] =
     items.items match {
       case item :: Nil =>
         request
           .post(baseUri)
           .body(item)
-          .response(asJson[File])
+          .response(asJson[Either[CdpApiError[CogniteId], File]])
           .mapResponse {
             case Left(value) => throw value.error
-            case Right(value) => Seq(value)
+            case Right(Left(cdpApiError)) => throw cdpApiError.asException(uri"$baseUri/byids")
+            case Right(Right(value)) => Seq(value)
           }
           .send()
       case _ => throw new RuntimeException("Files only support creating one file per call")
