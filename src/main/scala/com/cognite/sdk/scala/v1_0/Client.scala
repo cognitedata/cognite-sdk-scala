51ab9e3adc50dd2f625a572dbf415b875744a8e5
Joel Wilsson <joel.wilsson@cognite.com>
2019-06-14 15:02:50 +0200

    Make the default client use a default implicit sttpBackend

--- src/main/scala/com/cognite/sdk/scala/v1_0/Client.scala
+++ src/main/scala/com/cognite/sdk/scala/v1_0/Client.scala
@@ -5,7 +5,7 @@ import com.softwaremill.sttp._
 
 import scala.concurrent.duration._
 
-final class Client[F[_]](implicit auth: Auth, sttpBackend: SttpBackend[F, _]) {
+class GenericClient[F[_], S](implicit auth: Auth, sttpBackend: SttpBackend[F, S]) {
   val project: String = auth.project.getOrElse {
     implicit val sttpBackend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend(
       options = SttpBackendOptions.connectionTimeout(90.seconds)
@@ -29,3 +29,5 @@ final class Client[F[_]](implicit auth: Auth, sttpBackend: SttpBackend[F, _]) {
   def rawTables(database: String): RawTables[F] = new RawTables(project, database)
   def rawRows(database: String, table: String): RawRows[F] = new RawRows(project, database, table)
 }
+
+final class Client(implicit sttpBackend: SttpBackend[Id, Nothing]) extends GenericClient[Id, Nothing]()
