b46bba4750c6fb38b1a8e9fbeb05cb7d2cc8c638
Eric Brattli <eric@brattli.net>
2019-06-28 09:14:39 +0200

    Add support for threeD revisions

--- src/main/scala/com/cognite/sdk/scala/v1/threeD.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/threeD.scala
@@ -47,3 +47,62 @@ class ThreeDModels[F[_]](project: String)(implicit auth: Auth, sttpBackend: Sttp
       }
       .send()
 }
+
+final case class Camera(
+    target: Option[Array[Double]],
+    position: Option[Array[Double]]
+)
+
+final case class ThreeDRevision(
+    id: Long,
+    fileId: Long,
+    published: Boolean,
+    rotation: Option[Array[Double]] = None,
+    camera: Option[Camera] = None,
+    status: String,
+    metadata: Option[Map[String, String]] = None,
+    thumbnailThreedFileId: Option[Long] = None,
+    thumbnailURL: Option[String] = None,
+    assetMappingCount: Long,
+    createdTime: Long
+) extends WithId[Long]
+
+final case class CreateThreeDRevision(
+    published: Boolean,
+    rotation: Option[Array[Double]] = None,
+    metadata: Option[Map[String, String]] = None,
+    camera: Option[Camera] = None,
+    fileId: Long
+)
+
+class ThreeDRevisions[F[_]](project: String, modelId: Long)(
+    implicit auth: Auth,
+    sttpBackend: SttpBackend[F, _]
+) extends ReadWritableResource[
+      ThreeDRevision,
+      CreateThreeDRevision,
+      F,
+      Id,
+      CogniteId,
+      Long
+    ]
+    with ResourceV1[F] {
+  override val baseUri =
+    uri"https://api.cognitedata.com/api/v1/projects/$project/3d/models/$modelId/revisions"
+
+  implicit val errorOrUnitDecoder: Decoder[Either[CdpApiError[CogniteId], Unit]] =
+    EitherDecoder.eitherDecoder[CdpApiError[CogniteId], Unit]
+  def deleteByIds(ids: Seq[Long]): F[Response[Unit]] =
+    // TODO: group deletes by max deletion request size
+    //       or assert that length of `ids` is less than max deletion request size
+    request
+      .post(uri"$baseUri/delete")
+      .body(Items(ids.map(toInternalId)))
+      .response(asJson[Either[CdpApiError[CogniteId], Unit]])
+      .mapResponse {
+        case Left(value) => throw value.error
+        case Right(Left(cdpApiError)) => throw cdpApiError.asException(uri"$baseUri/delete")
+        case Right(Right(_)) => ()
+      }
+      .send()
+}
