7ec093aca4c98f59b0b54d0b165c89390756b845
Joel Wilsson <joel.wilsson@cognite.com>
2019-07-16 16:14:12 +0200

    Remove support for 0.6

--- src/main/scala/com/cognite/sdk/scala/v06/resources/ResourceV0_6.scala
+++
@@ -1,8 +0,0 @@
-package com.cognite.sdk.scala.v06.resources
-
-import com.cognite.sdk.scala.common.Resource
-
-trait ResourceV0_6[F[_]] extends Resource[F, Long, Long] {
-  def toInternalId(id: Long): Long = id
-  //implicit val idEncoder: Encoder[Long] = Encoder.encodeLong
-}
