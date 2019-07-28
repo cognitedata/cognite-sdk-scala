f67cbbc9aa37dafe9c3b2df1c041f89edf85576a
Joel Wilsson <joel.wilsson@gmail.com>
2019-07-28 18:36:24 +0200

    Move non-string and string data points tests to separate test suites
    This allows us to run them in parallel, significantly reducing the
    time it takes to run our tests.

--- src/test/scala/com/cognite/sdk/scala/common/StringDataPointsResourceBehaviors.scala
+++ src/test/scala/com/cognite/sdk/scala/common/StringDataPointsResourceBehaviors.scala
@@ -1,3 +1,35 @@
-package com.cognite.sdk.scala.common trait StringDataPointsResourceBehaviors {
+package com.cognite.sdk.scala.common
 
+import java.time.Instant
+
+import com.softwaremill.sttp.Id
+import org.scalatest.{FlatSpec, Matchers}
+
+trait StringDataPointsResourceBehaviors[I] extends Matchers { this: FlatSpec =>
+  private val startTime = System.currentTimeMillis()
+  private val endTime = startTime + 20*1000
+  private val testStringDataPoints = (startTime to endTime by 1000).map(t =>
+    StringDataPoint(Instant.ofEpochMilli(t), math.random.toString))
+
+  def withStringTimeSeriesId(testCode: I => Any): Unit
+
+  def stringDataPointsResource(dataPoints: DataPointsResource[Id, I]): Unit = {
+    it should "be possible to insert and delete string data points" in withStringTimeSeriesId { stringTimeSeriesId =>
+      dataPoints.insertStringsById(stringTimeSeriesId, testStringDataPoints)
+
+      Thread.sleep(2000)
+      val points = dataPoints.queryStringsById(stringTimeSeriesId, startTime, endTime + 1)
+      points should have size testStringDataPoints.size.toLong
+
+      val latest = dataPoints.getLatestStringDataPointById(stringTimeSeriesId)
+      latest.isDefined should be (true)
+      val latestPoint = latest.get
+      testStringDataPoints.toList should contain (latestPoint)
+
+      dataPoints.deleteRangeById(stringTimeSeriesId, startTime, endTime + 1)
+      Thread.sleep(10000)
+      val pointsAfterDelete = dataPoints.queryById(stringTimeSeriesId, startTime, endTime + 1)
+      pointsAfterDelete should have size 0
+    }
+  }
 }
--- src/test/scala/com/cognite/sdk/scala/v1/StringDataPointsTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/StringDataPointsTest.scala
@@ -1,3 +1,23 @@
-package com.cognite.sdk.scala.v1 class StringDataPointsTest {
+package com.cognite.sdk.scala.v1
 
+import java.util.UUID
+
+import cats.{Functor, Id}
+import com.cognite.sdk.scala.common.{SdkTest, StringDataPointsResourceBehaviors}
+
+class StringDataPointsTest extends SdkTest with StringDataPointsResourceBehaviors[Long] {
+  private val client = new GenericClient()(implicitly[Functor[Id]], auth, sttpBackend)
+
+  override def withStringTimeSeriesId(testCode: Long => Any): Unit = {
+    val timeSeriesId = client.timeSeries.createFromRead(
+      Seq(TimeSeries(name = s"string-data-points-test-${UUID.randomUUID().toString}", isString = true))
+    ).head.id
+    try {
+      val _ = testCode(timeSeriesId)
+    } finally {
+      client.timeSeries.deleteByIds(Seq(timeSeriesId))
+    }
+  }
+
+  it should behave like stringDataPointsResource(client.dataPoints)
 }
