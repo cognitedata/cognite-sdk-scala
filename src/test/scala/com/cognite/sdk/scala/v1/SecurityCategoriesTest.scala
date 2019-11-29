9997278e124c6aaa5bb71f7310eb225609747cd5
Joel Wilsson <joel.wilsson@cognite.com>
2019-11-29 14:42:30 +0100

    Comment out tests to ignore, @Ignore doesn't work

--- src/test/scala/com/cognite/sdk/scala/v1/SecurityCategoriesTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/SecurityCategoriesTest.scala
@@ -1,10 +1,12 @@
 package com.cognite.sdk.scala.v1
 
 import com.cognite.sdk.scala.common.{ReadBehaviours, SdkTest}
-import org.scalatest.Ignore
-
-@Ignore
+// FIXME: Not sure why @Ignore isn't working. "behave like" doesn't seem to support ignore.
+//import org.scalatest.Ignore
+//
+//@Ignore
 class SecurityCategoriesTest extends SdkTest with ReadBehaviours {
-  // We don't have any security categories in publicdata, and lack permissions to list them in greenfield
-  "SecurityCategories" should behave like readable(client.securityCategories, supportsLimit = false)
+  // TODO: We don't have any security categories in publicdata,
+  //  and lack permissions to list them in greenfield
+//  "SecurityCategories" should behave like readable(client.securityCategories, supportsLimit = false)
 }
