79e79a2f066a738c4263e95753cf5d96c6db6b8a
Joel Wilsson <joel.wilsson@cognite.com>
2019-11-29 13:04:45 +0100

    Use ScalaTest ignore instead of commenting out tests

--- src/test/scala/com/cognite/sdk/scala/v1/ProjectTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/ProjectTest.scala
@@ -3,9 +3,9 @@ package com.cognite.sdk.scala.v1
 import com.cognite.sdk.scala.common.SdkTest
 
 class ProjectTest extends SdkTest {
-  // We don't have sufficient permissions in publicdata or Greenfield
-//  "Project" should "be retrievable and correct" in {
-//    greenfieldClient.project.name shouldNot be (empty)
-//    greenfieldClient.project.urlName shouldNot be (empty)
-//  }
+  // TODO: We don't have sufficient permissions in publicdata or Greenfield
+  "Project" should "be retrievable and correct" ignore {
+    greenfieldClient.project.name shouldNot be (empty)
+    greenfieldClient.project.urlName shouldNot be (empty)
+  }
 }
--- src/test/scala/com/cognite/sdk/scala/v1/ServiceAccountsTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/ServiceAccountsTest.scala
@@ -1,8 +1,10 @@
 package com.cognite.sdk.scala.v1
 
 import com.cognite.sdk.scala.common.{ReadBehaviours, SdkTest}
+import org.scalatest.Ignore
 
+// TODO: We don't have permissions to list service accounts
+@Ignore
 class ServiceAccountsTest extends SdkTest with ReadBehaviours {
-  // We don't have permissions to list service accounts
-  //"ServiceAccounts" should behave like readable(client.serviceAccounts)
+  "ServiceAccounts" should behave like readable(client.serviceAccounts)
 }
