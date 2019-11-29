d86b55178c86a85ad2acc99461bafab2b84204ae
Joel Wilsson <joel.wilsson@cognite.com>
2019-11-29 13:47:46 +0100

    Rename projects.scala to iam.scala, ignore SecurityCategories test

--- src/test/scala/com/cognite/sdk/scala/v1/SecurityCategoriesTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/SecurityCategoriesTest.scala
@@ -1,8 +1,10 @@
 package com.cognite.sdk.scala.v1
 
 import com.cognite.sdk.scala.common.{ReadBehaviours, SdkTest}
+import org.scalatest.Ignore
 
+@Ignore
 class SecurityCategoriesTest extends SdkTest with ReadBehaviours {
   // We don't have any security categories in publicdata, and lack permissions to list them in greenfield
-  //"SecurityCategories" should behave like readable(client.securityCategories, supportsLimit = false)
+  "SecurityCategories" should behave like readable(client.securityCategories, supportsLimit = false)
 }
