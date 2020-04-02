18f76473a8489aa3da4fb5c2165a1638fb535854
Emil Sandstø <emilalexer@hotmail.com>
2020-04-02 13:12:46 +0200

    Remove implicit ExecutionContext
    We should not force our users to use the implicit ExecutionContext found in the package object. Instead our users should provide their own. The following caused errors before:
    ```
    import com.cognite.sdk.scala.v1._
    import scala.concurrent.ExecutionContext.Implicits.global
    implicitly[ExecutionContext]
    ```

--- src/main/scala/com/cognite/sdk/scala/v1/package.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/package.scala
@@ -1,18 +1,12 @@
 package com.cognite.sdk.scala
 
-import java.util.concurrent.Executors
-
 import scala.concurrent.duration._
 
 import cats.Id
 import com.softwaremill.sttp.{HttpURLConnectionBackend, SttpBackend}
 import com.cognite.sdk.scala.common.{GzipSttpBackend, RetryingBackend}
 
-import scala.concurrent.ExecutionContext
-
 package object v1 {
-  implicit val executionContext: ExecutionContext =
-    ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
   implicit val sttpBackend: SttpBackend[Id, Nothing] =
     new RetryingBackend[Id, Nothing](
       new GzipSttpBackend[Id, Nothing](
