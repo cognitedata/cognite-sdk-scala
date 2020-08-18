b6d68d5a2aae3824a086cddfa2c891b635248714
Emil Sandstø <emil.sandsto@gmail.com>
2020-08-18 22:16:35 +0200

    Add Labels resource
    Label filtering should be added as separate feature to Assets and Files.

---
+++ src/main/scala/com/cognite/sdk/scala/v1/labels.scala
@@ -0,0 +1,20 @@
+package com.cognite.sdk.scala.v1
+
+import java.time.Instant
+import com.cognite.sdk.scala.common._
+
+final case class Label(
+    externalId: String,
+    name: String,
+    description: Option[String] = None,
+    createdTime: Instant = Instant.ofEpochMilli(0)
+) extends WithRequiredExternalId
+    with WithCreatedTime
+
+final case class LabelCreate(
+    externalId: String,
+    name: String,
+    description: Option[String] = None
+) extends WithRequiredExternalId
+
+final case class LabelsFilter(name: Option[String] = None, externalIdPrefix: Option[String] = None)
