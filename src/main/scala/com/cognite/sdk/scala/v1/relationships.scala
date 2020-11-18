4e9dd126fb75ac71f696d7038bc956d1a6b18cb3
Emel Varol <emel.varol@cognite.com>
2020-11-18 15:20:02 +0100

    Started relationships support implementation.

---
+++ src/main/scala/com/cognite/sdk/scala/v1/relationships.scala
@@ -0,0 +1,25 @@
+// Copyright 2020 Cognite AS
+// SPDX-License-Identifier: Apache-2.0
+
+package com.cognite.sdk.scala.v1
+
+import java.time.Instant
+import com.cognite.sdk.scala.common._
+
+final case class Relationship(
+    id: Long = 0,
+    externalId: Option[String] = None,
+    sourceExternalId: String = "",
+    sourceType: String = "",
+    targetExternalId: String = "",
+    targetType: String = "",
+    confidence: Float = 0,
+    startTime: Option[Instant] = None,
+    endTime: Option[Instant] = None,
+    createdTime: Instant = Instant.ofEpochMilli(0),
+    lastUpdatedTime: Instant = Instant.ofEpochMilli(0),
+    dataSetId: Option[Long] = None,
+    labels: Option[Seq[Map[String, String]]] = None
+) extends WithId[Long]
+    with WithExternalId
+    with WithCreatedTime
---
+++ src/main/scala/com/cognite/sdk/scala/v1/resources/relationships.scala
@@ -0,0 +1,63 @@
+// Copyright 2020 Cognite AS
+// SPDX-License-Identifier: Apache-2.0
+
+package com.cognite.sdk.scala.v1.resources
+
+import com.cognite.sdk.scala.common._
+import com.cognite.sdk.scala.v1._
+import com.softwaremill.sttp._
+import io.circe.derivation.deriveDecoder
+import io.circe.Decoder
+
+class Relationships[F[_]](val requestSession: RequestSession[F])
+    extends WithRequestSession[F]
+    with PartitionedReadable[Relationship, F]
+    with RetrieveByIdsWithIgnoreUnknownIds[Relationship, F]
+    with RetrieveByExternalIdsWithIgnoreUnknownIds[Relationship, F] {
+  import Relationships._
+  override val baseUrl = uri"${requestSession.baseUrl}/relationships"
+
+  override private[sdk] def readWithCursor(
+      cursor: Option[String],
+      limit: Option[Int],
+      partition: Option[Partition]
+  ): F[ItemsWithCursor[Relationship]] =
+    Readable.readWithCursor(
+      requestSession,
+      baseUrl,
+      cursor,
+      limit,
+      partition,
+      Constants.defaultBatchSize
+    )
+
+  override def retrieveByIds(
+      ids: Seq[Long],
+      ignoreUnknownIds: Boolean
+  ): F[Seq[Relationship]] =
+    RetrieveByIdsWithIgnoreUnknownIds.retrieveByIds(
+      requestSession,
+      baseUrl,
+      ids,
+      ignoreUnknownIds
+    )
+
+  override def retrieveByExternalIds(
+      externalIds: Seq[String],
+      ignoreUnknownIds: Boolean
+  ): F[Seq[Relationship]] =
+    RetrieveByExternalIdsWithIgnoreUnknownIds.retrieveByExternalIds(
+      requestSession,
+      baseUrl,
+      externalIds,
+      ignoreUnknownIds
+    )
+}
+
+object Relationships {
+  implicit val relationshipDecoder: Decoder[Relationship] = deriveDecoder[Relationship]
+  implicit val relationshipItemsWithCursorDecoder: Decoder[ItemsWithCursor[Relationship]] =
+    deriveDecoder[ItemsWithCursor[Relationship]]
+  implicit val relationshipItemsDecoder: Decoder[Items[Relationship]] =
+    deriveDecoder[Items[Relationship]]
+}
