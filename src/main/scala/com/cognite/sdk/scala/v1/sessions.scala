8975b4aebf99f5a1bbc73e7b0b99ab8b03408cea
vuhainguyen <92026183+vuhainguyen@users.noreply.github.com>
2021-11-03 08:20:18 +0100

    Add support for sessions API (#399)
    * Add support for sessions API
    
    CDF-13661

---
+++ src/main/scala/com/cognite/sdk/scala/v1/sessions.scala
@@ -0,0 +1,46 @@
+// Copyright 2020 Cognite AS
+// SPDX-License-Identifier: Apache-2.0
+
+package com.cognite.sdk.scala.v1
+
+final case class Session(
+    id: Long,
+    `type`: String,
+    status: String,
+    nonce: String,
+    clientId: String
+)
+
+final case class SessionCreateWithToken(
+    tokenExchange: Boolean = true
+)
+
+final case class SessionCreateWithCredential(
+    clientId: String,
+    clientSecret: String
+)
+
+final case class SessionList(
+    id: Long,
+    `type`: String,
+    status: String,
+    creationTime: Long,
+    expirationTime: Long,
+    clientId: String
+)
+
+final case class BindSessionRequest(
+    nonce: String
+)
+
+final case class RefreshSessionRequest(
+    sessionKey: String
+)
+
+final case class SessionTokenResponse(
+    id: Long,
+    accessToken: String,
+    expiresIn: Long,
+    mustRefreshIn: Long,
+    sessionKey: Option[String]
+)
