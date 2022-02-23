02847621b0937dcb8edeaca0ddd81b4b9b4e0a51
vuhainguyen <vu.nguyen@cognite.com>
2022-02-23 17:07:02 +0100

    Fix compilation for scala 2.13

--- src/main/scala/com/cognite/sdk/scala/v1/dataModels.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/dataModels.scala
@@ -36,14 +36,14 @@ final case class DataModelInstance(
 sealed trait DataModelInstanceFilter
 
 sealed trait DMIBoolFilter extends DataModelInstanceFilter
-case class DMIAndFilter(and: Seq[DataModelInstanceFilter]) extends DMIBoolFilter
-case class DMIOrFilter(or: Seq[DataModelInstanceFilter]) extends DMIBoolFilter
-case class DMINotFilter(not: DataModelInstanceFilter) extends DMIBoolFilter
+final case class DMIAndFilter(and: Seq[DataModelInstanceFilter]) extends DMIBoolFilter
+final case class DMIOrFilter(or: Seq[DataModelInstanceFilter]) extends DMIBoolFilter
+final case class DMINotFilter(not: DataModelInstanceFilter) extends DMIBoolFilter
 
 sealed trait DMILeafFilter extends DataModelInstanceFilter
-case class DMIEqualsFilter(property: Seq[String], value: Json) extends DMILeafFilter
-case class DMIInFilter(property: Seq[String], values: Seq[Json]) extends DMILeafFilter
-case class DMIRangeFilter(
+final case class DMIEqualsFilter(property: Seq[String], value: Json) extends DMILeafFilter
+final case class DMIInFilter(property: Seq[String], values: Seq[Json]) extends DMILeafFilter
+final case class DMIRangeFilter(
     property: Seq[String],
     gte: Option[Json] = None,
     gt: Option[Json] = None,
@@ -53,13 +53,15 @@ case class DMIRangeFilter(
   require(
     !(gte.isDefined && gt.isDefined) && // can't have both upper bound in the same time
       !(lte.isDefined && lt.isDefined) && // can't have both lower bound in the same time
-      (gte.isDefined || gt.isDefined || lte.isDefined || lt.isDefined) // at least oue bound must be defined
+      (gte.isDefined || gt.isDefined || lte.isDefined || lt.isDefined) // at least one bound must be defined
   )
 }
-case class DMIPrefixFilter(property: Seq[String], value: Json) extends DMILeafFilter
-case class DMIExistsFilter(property: Seq[String]) extends DMILeafFilter
-case class DMIContainsAnyFilter(property: Seq[String], values: Seq[Json]) extends DMILeafFilter
-case class DMIContainsAllFilter(property: Seq[String], values: Seq[Json]) extends DMILeafFilter
+final case class DMIPrefixFilter(property: Seq[String], value: Json) extends DMILeafFilter
+final case class DMIExistsFilter(property: Seq[String]) extends DMILeafFilter
+final case class DMIContainsAnyFilter(property: Seq[String], values: Seq[Json])
+    extends DMILeafFilter
+final case class DMIContainsAllFilter(property: Seq[String], values: Seq[Json])
+    extends DMILeafFilter
 
 final case class DataModelInstanceQuery(
     modelExternalId: String,
--- src/main/scala/com/cognite/sdk/scala/v1/resources/dataModelInstances.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/resources/dataModelInstances.scala
@@ -34,7 +34,8 @@ class DataModelInstances[F[_]](val requestSession: RequestSession[F])
       inputQuery: DataModelInstanceQuery
   ): F[ItemsWithCursor[DataModelInstanceQueryResponse]] = {
     implicit val printer: Printer = Printer.noSpaces.copy(dropNullValues = true)
-    println(s"Coucou JsonQuery is  = ${inputQuery.asJson}")
+    // VH TODO remove this println
+    println(s"Coucou JsonQuery is  = ${inputQuery.asJson.toString()}")
     requestSession.post[ItemsWithCursor[DataModelInstanceQueryResponse], ItemsWithCursor[
       DataModelInstanceQueryResponse
     ], DataModelInstanceQuery](
