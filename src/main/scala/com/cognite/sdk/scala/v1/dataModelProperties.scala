f0cb22c2db46475db38aa7294b9824a720b933ea
Emel Varol <emel.varol@cognite.com>
2022-03-21 15:48:24 +0100

    Fix value

--- src/main/scala/com/cognite/sdk/scala/v1/dataModelProperties.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/dataModelProperties.scala
@@ -4,33 +4,17 @@
 package com.cognite.sdk.scala.v1
 
 sealed trait PropertyType[T] {
-  val getValue: T
+  val value: T
 }
 
 sealed trait PropertyTypePrimitive[T] extends PropertyType[T]
-final case class BooleanProperty(value: Boolean) extends PropertyTypePrimitive[Boolean] {
-  override val getValue: Boolean = value
-}
-final case class Int32Property(value: Int) extends PropertyTypePrimitive[Int] {
-  override val getValue: Int = value
-}
-
-final case class Int64Property(value: Long) extends PropertyTypePrimitive[Long] {
-  override val getValue: Long = value
-}
-
-final case class Float32Property(value: Float) extends PropertyTypePrimitive[Float] {
-  override val getValue: Float = value
-}
-
-final case class Float64Property(value: Double) extends PropertyTypePrimitive[Double] {
-  override val getValue: Double = value
-}
-final case class StringProperty(value: String) extends PropertyTypePrimitive[String] {
-  override val getValue: String = value
-}
-
-final case class ArrayProperty[+A <: PropertyTypePrimitive[A]](values: Vector[A])
-    extends PropertyType[Vector[A]] {
-  override val getValue: Vector[A] = values
+final case class BooleanProperty(override val value: Boolean) extends PropertyTypePrimitive[Boolean]
+final case class Int32Property(override val value: Int) extends PropertyTypePrimitive[Int]
+final case class Int64Property(override val value: Long) extends PropertyTypePrimitive[Long]
+final case class Float32Property(override val value: Float) extends PropertyTypePrimitive[Float]
+final case class Float64Property(override val value: Double) extends PropertyTypePrimitive[Double]
+final case class StringProperty(override val value: String) extends PropertyTypePrimitive[String]
+final case class ArrayProperty[+A <: PropertyTypePrimitive[T], T](values: Vector[A])
+    extends PropertyType[Vector[T]] {
+  override val value: Vector[T] = values.map(_.value)
 }
