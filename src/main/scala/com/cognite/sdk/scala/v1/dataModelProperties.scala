a36d3b5a97e70298c3dddf1d903089ef827cbbcd
Emel Varol <emel.varol@cognite.com>
2022-03-21 15:31:36 +0100

    Add a generic function to extract data from property type

--- src/main/scala/com/cognite/sdk/scala/v1/dataModelProperties.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/dataModelProperties.scala
@@ -3,14 +3,34 @@
 
 package com.cognite.sdk.scala.v1
 
-sealed trait PropertyType
+sealed trait PropertyType[T] {
+  val getValue: T
+}
 
-sealed trait PropertyTypePrimitive extends PropertyType
-final case class BooleanProperty(value: Boolean) extends PropertyTypePrimitive
-final case class Int32Property(value: Int) extends PropertyTypePrimitive
-final case class Int64Property(value: Long) extends PropertyTypePrimitive
-final case class Float32Property(value: Float) extends PropertyTypePrimitive
-final case class Float64Property(value: Double) extends PropertyTypePrimitive
-final case class StringProperty(value: String) extends PropertyTypePrimitive
+sealed trait PropertyTypePrimitive[T] extends PropertyType[T]
+final case class BooleanProperty(value: Boolean) extends PropertyTypePrimitive[Boolean] {
+  override val getValue: Boolean = value
+}
+final case class Int32Property(value: Int) extends PropertyTypePrimitive[Int] {
+  override val getValue: Int = value
+}
 
-final case class ArrayProperty[+A <: PropertyTypePrimitive](values: Vector[A]) extends PropertyType
+final case class Int64Property(value: Long) extends PropertyTypePrimitive[Long] {
+  override val getValue: Long = value
+}
+
+final case class Float32Property(value: Float) extends PropertyTypePrimitive[Float] {
+  override val getValue: Float = value
+}
+
+final case class Float64Property(value: Double) extends PropertyTypePrimitive[Double] {
+  override val getValue: Double = value
+}
+final case class StringProperty(value: String) extends PropertyTypePrimitive[String] {
+  override val getValue: String = value
+}
+
+final case class ArrayProperty[+A <: PropertyTypePrimitive[A]](values: Vector[A])
+    extends PropertyType[Vector[A]] {
+  override val getValue: Vector[A] = values
+}
