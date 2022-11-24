0810cb13304d540a8a545c0d12bda63b019da017
vuhainguyen <vu.nguyen@cognite.com>
2022-11-24 16:10:12 +0100

    Add support for spaces with DMS v3
    CDF-17171

--- src/main/scala/com/cognite/sdk/scala/v1/resources/spaces.scala
+++ src/main/scala/com/cognite/sdk/scala/v1/resources/spaces.scala
@@ -5,8 +5,8 @@ package com.cognite.sdk.scala.v1.resources
 
 import com.cognite.sdk.scala.common._
 import com.cognite.sdk.scala.v1._
-import io.circe.generic.semiauto.deriveDecoder
-import io.circe.{Decoder, Printer}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder, Printer}
 import sttp.client3._
 import sttp.client3.circe._
 
@@ -14,46 +14,49 @@ class Spaces[F[_]](val requestSession: RequestSession[F])
     extends WithRequestSession[F]
     with BaseUrl {
   import Spaces._
-  override val baseUrl = uri"${requestSession.baseUrl}/datamodelstorage/spaces"
+  override val baseUrl = uri"${requestSession.baseUrl}/models/spaces"
 
-  def createItems(externalIds: Seq[String]): F[Seq[String]] = {
+  def createItems(spaces: Seq[SpaceCreate]): F[Seq[Space]] = {
     implicit val printer: Printer = Printer.noSpaces.copy(dropNullValues = true)
-    requestSession.post[Seq[String], Items[CogniteExternalId], Items[CogniteId]](
-      Items(externalIds.map(CogniteExternalId(_))),
+    requestSession.post[Seq[Space], Items[Space], Items[SpaceCreate]](
+      Items(spaces),
       uri"$baseUrl",
-      value => value.items.map(_.externalId)
+      value => value.items
     )
   }
 
-  def deleteItems(externalIds: Seq[String]): F[Unit] =
-    requestSession.post[Unit, Unit, Items[CogniteId]](
-      Items(externalIds.map(CogniteExternalId(_))),
+  def deleteItems(spaces: Seq[String]): F[Unit] = {
+    implicit val printer: Printer = Printer.noSpaces.copy(dropNullValues = true)
+    requestSession.post[Unit, Unit, Items[SpaceId]](
+      Items(spaces.map(SpaceId)),
       uri"$baseUrl/delete",
       _ => ()
     )
+  }
 
-  def list(): F[Seq[String]] =
-    requestSession.post[Seq[String], Items[CogniteExternalId], Unit](
-      (),
-      uri"$baseUrl/list",
-      value => value.items.map(_.externalId)
-    )
-
-  def retrieveByExternalIds(
-      externalIds: Seq[String]
-  ): F[Seq[String]] =
+  def retrieveBySpaceIds(spaces: Seq[String]): F[Seq[Space]] = {
+    implicit val printer: Printer = Printer.noSpaces.copy(dropNullValues = true)
     requestSession
-      .post[Seq[String], Items[CogniteExternalId], Items[CogniteId]](
-        Items(
-          externalIds.map(CogniteExternalId(_))
-        ),
+      .post[Seq[Space], Items[Space], Items[SpaceId]](
+        Items(spaces.map(SpaceId)),
         uri"$baseUrl/byids",
-        value => value.items.map(_.externalId)
+        value => value.items
       )
+  }
 
 }
 
 object Spaces {
-  implicit val externalIdItemsDecoder: Decoder[Items[CogniteExternalId]] =
-    deriveDecoder[Items[CogniteExternalId]]
+  implicit val spaceCreateEncoder: Encoder[SpaceCreate] = deriveEncoder[SpaceCreate]
+  implicit val spaceCreateItemsEncoder: Encoder[Items[SpaceCreate]] =
+    deriveEncoder[Items[SpaceCreate]]
+
+  implicit val spaceIdEncoder: Encoder[SpaceId] = deriveEncoder[SpaceId]
+  implicit val spaceIdItemsEncoder: Encoder[Items[SpaceId]] = deriveEncoder[Items[SpaceId]]
+
+  implicit val spaceEncoder: Encoder[Space] = deriveEncoder[Space]
+  implicit val spaceItemsEncoder: Encoder[Items[Space]] = deriveEncoder[Items[Space]]
+
+  implicit val spaceDecoder: Decoder[Space] = deriveDecoder[Space]
+  implicit val spaceItemsDecoder: Decoder[Items[Space]] = deriveDecoder[Items[Space]]
 }
