d4fe811f145d67a5a9a718a8bffd6557f1ccc507
Thorkild Stray <thorkild.stray@cognite.com>
2023-06-15 05:39:51 +0200

    Enable list suport for CDF references (all kinds)
    This has been available for a while in DMS.

--- src/test/scala/com/cognite/sdk/scala/v1/fdm/Utils.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/fdm/Utils.scala
@@ -43,11 +43,10 @@ object Utils {
       source = None),
     TimeSeriesReference(list = Some(false)),
     FileReference(list = Some(false)),
-    SequenceReference(list = Some(false))
-//TODO: Uncomment once the list types are released
-//    TimeSeriesReference(list = Some(true)),
-//    FileReference(list = Some(true)),
-//    SequenceReference(list = Some(true)),
+    SequenceReference(list = Some(false)),
+    TimeSeriesReference(list = Some(true)),
+    FileReference(list = Some(true)),
+    SequenceReference(list = Some(true))
   )
 
   val AllPropertyDefaultValues: List[PropertyDefaultValue] = List(
