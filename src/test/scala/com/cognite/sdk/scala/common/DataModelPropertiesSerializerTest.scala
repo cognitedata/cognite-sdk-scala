86a97d730f6b06492b47c8812aed04deaf717090
Dmitry Ivankov <dmitry.ivankov@cognite.com>
2023-08-25 09:53:01 +0200

    tests

--- src/test/scala/com/cognite/sdk/scala/common/DataModelPropertiesSerializerTest.scala
+++ src/test/scala/com/cognite/sdk/scala/common/DataModelPropertiesSerializerTest.scala
@@ -27,6 +27,7 @@ import java.time.{LocalDate, ZoneOffset, ZonedDateTime}
     "org.wartremover.warts.IsInstanceOf"
   )
 )
+@deprecated
 class DataModelPropertiesSerializerTest extends AnyWordSpec with Matchers {
 
   implicit val dataModelInstanceQueryResponseDecoder: Decoder[DataModelInstanceQueryResponse] = {
