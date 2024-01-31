ac89a76eb69d879b85d1e4ff5b867ea9f87e4a23
Andreas Korneliussen <andreas.korneliussen@cognite.com>
2024-01-31 13:03:28 +0100

    add coverage for edge table expression

--- src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/InstanceSyncSerDerTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/InstanceSyncSerDerTest.scala
@@ -19,7 +19,7 @@ import org.scalatest.wordspec.AnyWordSpec
 )
 class InstanceSyncSerDerTest extends AnyWordSpec with Matchers {
 
-  "Instance sync Request" should {
+  "Instance sync nodes Request" should {
 
     "be encoded to json" in {
       val viewReference = ViewReference("spaceId", "viewId", "version1")
@@ -49,6 +49,65 @@ class InstanceSyncSerDerTest extends AnyWordSpec with Matchers {
           |          ]
           |        },
           |        "through" : null
+          |      }
+          |    }
+          |  },
+          |  "select" : {
+          |    "sync" : {
+          |      "sources" : [
+          |        {
+          |          "source" : {
+          |            "type" : "view",
+          |            "space" : "spaceId",
+          |            "externalId" : "viewId",
+          |            "version" : "version1"
+          |          },
+          |          "properties" : [
+          |            "*"
+          |          ]
+          |        }
+          |      ]
+          |    }
+          |  }
+          |}""".stripMargin
+
+      requestAsJson.toString() should (be(expectedJson))
+
+    }
+  }
+
+  "Instance sync edges Request" should {
+
+    "be encoded to json" in {
+      val viewReference = ViewReference("spaceId", "viewId", "version1")
+      val hasData = HasData(Seq(viewReference))
+      val request = InstanceSyncRequest(
+        `with` = Map("sync" -> TableExpression(edges = Option(EdgeTableExpression(filter = Option(hasData))))),
+        select = Map("sync" -> SelectExpression(sources = Seq(SourceSelector(source = viewReference, properties = List("*"))))))
+
+      val requestAsJson = request.asJson
+      val expectedJson =
+        """{
+          |  "with" : {
+          |    "sync" : {
+          |      "limit" : 1000,
+          |      "edges" : {
+          |        "from" : null,
+          |        "chainTo" : "destination",
+          |        "maxDistance" : null,
+          |        "direction" : "inwards",
+          |        "filter" : {
+          |          "hasData" : [
+          |            {
+          |              "type" : "view",
+          |              "space" : "spaceId",
+          |              "externalId" : "viewId",
+          |              "version" : "version1"
+          |            }
+          |          ]
+          |        },
+          |        "nodeFilter" : null,
+          |        "terminationFilter" : null
           |      }
           |    }
           |  },
