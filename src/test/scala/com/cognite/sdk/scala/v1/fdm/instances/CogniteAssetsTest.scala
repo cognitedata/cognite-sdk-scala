85b62d8adc9b83bfcbeb4169942b1dd2eeeb129a
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2024-11-27 13:18:34 +0100

    fmt

--- src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/CogniteAssetsTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/CogniteAssetsTest.scala
@@ -1,7 +1,7 @@
 package com.cognite.sdk.scala.v1.fdm.instances
 
-import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import com.cognite.sdk.scala.common.RetryWhile
 import com.cognite.sdk.scala.v1.fdm.Utils
 import com.cognite.sdk.scala.v1.fdm.instances.InstanceDeletionRequest.NodeDeletionRequest
 import com.cognite.sdk.scala.v1.fdm.instances.NodeOrEdgeCreate.NodeWrite
@@ -10,8 +10,6 @@ import com.cognite.sdk.scala.v1.{CommonDataModelTestHelper, File, FileDownloadIn
 import sttp.client3.UriContext
 
 import java.io.{BufferedInputStream, FileInputStream}
-import scala.annotation.tailrec
-import scala.util.Try
 
 @SuppressWarnings(
   Array(
@@ -20,8 +18,7 @@ import scala.util.Try
     "org.wartremover.warts.Equals"
   )
 )
-class CogniteAssetsTest extends CommonDataModelTestHelper {
-  private val defaultAttemptCount: Int = 10
+class CogniteAssetsTest extends CommonDataModelTestHelper with RetryWhile {
 
   it should "make it possible to retrieve file and associated upload link and download link using instance id" in {
 
@@ -43,13 +40,13 @@ class CogniteAssetsTest extends CommonDataModelTestHelper {
     )
     val createdItem = testClient.instances.createItems(testFile).unsafeRunSync()
     val retrievedItem =
-      retry[Seq[File]](() => testClient.files.retrieveByInstanceIds(Seq(instanceId)), defaultAttemptCount)
+      retry[Seq[File]](testClient.files.retrieveByInstanceIds(Seq(instanceId)).unsafeRunSync())
 
     val retrievedSingleItem =
-      retry[File](() => testClient.files.retrieveByInstanceId(instanceId), defaultAttemptCount)
+      retry[File](testClient.files.retrieveByInstanceId(instanceId).unsafeRunSync())
 
     val uploadLinkFile =
-      retry[File](() => testClient.files.uploadLink(FileUploadInstanceId(instanceId)), defaultAttemptCount)
+      retry[File](testClient.files.uploadLink(FileUploadInstanceId(instanceId)).unsafeRunSync())
 
     val file = new java.io.File("./src/test/scala/com/cognite/sdk/scala/v1/uploadTest.txt")
     val inputStream = new BufferedInputStream(
@@ -58,45 +55,28 @@ class CogniteAssetsTest extends CommonDataModelTestHelper {
       )
     )
     val fileSize = file.length()
-    uploadLinkFile.map(_.uploadUrl) match {
-      case Right(Some(uploadUrl)) =>
+    uploadLinkFile.uploadUrl match {
+      case Some(uploadUrl) =>
         testClient.requestSession.send { request =>
           request
             .contentLength(fileSize)
             .body(inputStream)
             .put(uri"$uploadUrl")
         }.unsafeRunSync()
-      case Right(None) => fail("no upload url returned by uploadLink")
-      case Left(e) => fail(e.getMessage)
+      case None => fail("no upload url returned by uploadLink")
     }
 
-    retryWithTest[FileDownloadLink](() => testClient.files.downloadLink(FileDownloadInstanceId(instanceId)), downloadLink => downloadLink.downloadUrl.nonEmpty, defaultAttemptCount, "empty download link")
+    retryWithExpectedResult[FileDownloadLink](testClient.files.downloadLink(FileDownloadInstanceId(instanceId)).unsafeRunSync(), downloadLink => downloadLink.downloadUrl should not be(empty))
 
     createdItem.headOption.flatMap(_.createdTime) shouldNot be(empty)
-    retrievedSingleItem.map(_.instanceId) should be(Right(Some(instanceId)))
-    retrievedItem.map(_.headOption.flatMap(_.instanceId)) should be(Right(Some(instanceId)))
+    retrievedSingleItem.instanceId should be(Some(instanceId))
+    retrievedItem.headOption.flatMap(_.instanceId) should be(Some(instanceId))
 
     testClient.instances.delete(Seq(NodeDeletionRequest(instanceId.space, instanceId.externalId))).unsafeRunSync()
   }
 
-  //Retries a request until it succeeds and the test passes or there is no attempt left
-  @tailrec
-  private final def retryWithTest[T](requestToAttempt: () => IO[T], test: T => Boolean, attemptsLeft: Int, testFailedMessage: String): Either[Throwable, T] = {
-    val result = Try(requestToAttempt().unsafeRunSync()).toEither
-
-    result match {
-      case Right(value) if test(value) => Right(value) // Successful test
-      case _ if attemptsLeft > 0 =>
-        Thread.sleep(500)
-        retryWithTest(requestToAttempt, test, attemptsLeft - 1, testFailedMessage) // Retry on failure
-      case Left(error) => Left(new IllegalStateException("Request still fails after exhausting retries", error))
-      case Right(_)    => Left(new IllegalStateException(s"Request still fails test after exhausting retries, test failed message: $testFailedMessage"))
-    }
+  private def retry[A](action: => A): A = {
+    retryWithExpectedResult[A](action, _ => succeed)
   }
-
-  private def retry[T](requestToAttempt: () => IO[T], attemptsLeft: Int): Either[Throwable, T] = {
-    retryWithTest[T](requestToAttempt, _ => true, attemptsLeft, "")
-  }
-
 
 }
