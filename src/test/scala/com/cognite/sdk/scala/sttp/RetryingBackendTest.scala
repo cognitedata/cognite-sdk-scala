8296f9e197ddda5b1007d3fd57edef369398ea5b
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2025-09-26 13:55:31 +0200

    fix test

--- src/test/scala/com/cognite/sdk/scala/sttp/RetryingBackendTest.scala
+++ src/test/scala/com/cognite/sdk/scala/sttp/RetryingBackendTest.scala
@@ -7,21 +7,23 @@ import sttp.monad.MonadError
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import sttp.capabilities.Effect
-import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
+import sttp.client3.impl.cats.implicits.monadError
 
 import java.net.UnknownHostException
 import scala.concurrent.duration.DurationInt
 
-@SuppressWarnings(Array("org.wartremover.warts.Var"))
-class FailingRetryingBackend[F[_], +P](delegate: SttpBackend[F, P]) extends SttpBackend[F, P] {
+@SuppressWarnings(Array("org.wartremover.warts.Var", "org.wartremover.warts.AsInstanceOf"))
+class FailingRetryingBackend[F[_], +P](val shouldSucceed: Boolean = false)(implicit val responseMonad: MonadError[F]) extends SttpBackend[F, P] {
   var callCount: Int = 0
 
   override def send[T, R >: P with Effect[F]](request: Request[T, R]): F[Response[T]] = {
     callCount += 1
-    responseMonad.error(new UnknownHostException("Connection failed"))
+    if(shouldSucceed)
+      responseMonad.unit(Response.ok(request.response.show.asInstanceOf[T]))
+    else
+      responseMonad.error(new UnknownHostException("Connection failed"))
   }
-  override def close(): F[Unit] = delegate.close()
-  override def responseMonad: MonadError[F] = delegate.responseMonad
+  override def close(): F[Unit] = responseMonad.unit(())
 }
 
 @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
@@ -29,12 +31,13 @@ class RetryingBackendTest extends AnyFlatSpec with Matchers {
   val port: Int = 50000 + (java.lang.Math.random() * 1000).toInt
 
   "RetryingBackend" should "retry failed requests multiple times" in {
-    val mockBackend = new FailingRetryingBackend[IO, Any](AsyncHttpClientCatsBackend[IO]().unsafeRunSync())
+    val mockBackend = new FailingRetryingBackend[IO, Any]
     val retryingBackend = new RetryingBackend[IO, Any](mockBackend, maxRetries = 2, initialRetryDelay = 1.millis)
 
+    //request doesn't matter, underlying backend just returns a failure
     val request = basicRequest
       .body("test")
-      .post(uri"http://unknownHost:$port/string")
+      .post(uri"http://host:$port/string")
 
     an[Exception] should be thrownBy {
       request.send(retryingBackend).unsafeRunSync()
@@ -42,5 +45,20 @@ class RetryingBackendTest extends AnyFlatSpec with Matchers {
     }
 
     mockBackend.callCount should be(3)
+  }
+
+  it should "not retry successful requests multiple times" in {
+    val mockBackend = new FailingRetryingBackend[IO, Any](shouldSucceed = true)
+    val retryingBackend = new RetryingBackend[IO, Any](mockBackend, maxRetries = 2, initialRetryDelay = 1.millis)
+
+    //request doesn't matter, underlying backend just returns a success
+    val request = basicRequest
+      .body("test")
+      .post(uri"http://host:$port/string")
+
+    request.send(retryingBackend).unsafeRunSync()
+
+
+    mockBackend.callCount shouldBe 1
   }
 }
