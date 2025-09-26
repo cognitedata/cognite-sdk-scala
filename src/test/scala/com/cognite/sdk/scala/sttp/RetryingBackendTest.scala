a1bd6c6b89176f7f2eab9ca867c6d1a64ab2ec1a
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2025-09-26 13:40:59 +0200

    small test

---
+++ src/test/scala/com/cognite/sdk/scala/sttp/RetryingBackendTest.scala
@@ -0,0 +1,46 @@
+package com.cognite.sdk.scala.sttp
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import sttp.client3._
+import sttp.monad.MonadError
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import sttp.capabilities.Effect
+import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
+
+import scala.concurrent.duration.DurationInt
+
+@SuppressWarnings(Array("org.wartremover.warts.Var"))
+class FailingRetryingBackend[F[_], +P](delegate: SttpBackend[F, P]) extends SttpBackend[F, P] {
+  var callCount: Int = 0
+
+  override def send[T, R >: P with Effect[F]](request: Request[T, R]): F[Response[T]] = {
+    callCount += 1
+    responseMonad.error(new java.net.ConnectException("Connection failed"))
+  }
+  override def close(): F[Unit] = delegate.close()
+  override def responseMonad: MonadError[F] = delegate.responseMonad
+}
+
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+class RetryingBackendTest extends AnyFlatSpec with Matchers {
+  val port: Int = 50000 + (java.lang.Math.random() * 1000).toInt
+
+  "RetryingBackend" should "retry failed requests multiple times" in {
+    val mockBackend = new FailingRetryingBackend[IO, Any](AsyncHttpClientCatsBackend[IO]().unsafeRunSync())
+    val retryingBackend = new RetryingBackend[IO, Any](mockBackend, maxRetries = 2, initialRetryDelay = 1.millis)
+
+    val request = basicRequest
+      .body("test")
+      .post(uri"http://unknownHost:$port/string")
+
+    an[Exception] should be thrownBy {
+      request.send(retryingBackend).unsafeRunSync()
+      ()
+    }
+
+    mockBackend.callCount should be >= 2
+    mockBackend.callCount should be <= 5
+  }
+}
