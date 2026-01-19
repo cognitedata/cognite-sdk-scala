2e7cde7d5bfb8a41d8b51f7058d60612c9c98461
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2026-01-19 13:38:30 +0100

    cleanup

--- src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/InstancesTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/InstancesTest.scala
@@ -287,8 +287,8 @@ class InstancesTest extends CommonDataModelTestHelper {
         )
 
         c.getMessage should contain (
-        """Graph query timed out. Reduce load or contention, or optimise your query. Hints from data modeling:
-          |The query is using one or more containers that doesn't have any indexes de.clared.""".stripMargin
+          """Graph query timed out. Reduce load or contention, or optimise your query. Hints from data modeling:
+            |The query is using one or more containers that doesn't have any indexes de.clared.""".stripMargin
         )
       }
       case _ => fail("unexpected type of exception when trying to get a 408 on list instance")
