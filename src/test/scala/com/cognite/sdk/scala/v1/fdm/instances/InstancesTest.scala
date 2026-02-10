6969d97e45850feae52afb589d0d42d021e40e48
Jacob-Eliat-Eliat <167076615+Jacob-Eliat-Eliat@users.noreply.github.com>
2026-02-10 15:14:02 +0100

    Remove hint from exception message (#925)
    * remove from exception message
    
    * fix test too
    
    * fmt

--- src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/InstancesTest.scala
+++ src/test/scala/com/cognite/sdk/scala/v1/fdm/instances/InstancesTest.scala
@@ -299,8 +299,7 @@ class InstancesTest extends CommonDataModelTestHelper {
         )
 
         c.getMessage should include (
-          """Graph query timed out. Reduce load or contention, or optimise your query. Hints from data modeling:
-            |The query is using one or more containers that doesn't have any indexes declared.""".stripMargin
+          """Graph query timed out. Reduce load or contention, or optimise your query.""".stripMargin
         )
       }
       case _ => fail("unexpected type of exception when trying to get a 408 on list instance")
